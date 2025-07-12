dbf20a8605f337371d3339901e8b070f70729e8c
akiko-npriko <hiddenshadows353@gmail.com>
2025-07-12 04:56:56 -0500

    Update raycastweaponbase.lua

--- lua/sc/units/weapons/raycastweaponbase.lua
+++ lua/sc/units/weapons/raycastweaponbase.lua
@@ -206,8 +206,16 @@ function RaycastWeaponBase.collect_hits(from, to, setup_data, weapon_unit)
 				break
 			elseif hit.unit:in_slot(shield_mask) and (not can_shoot_through_shield or (is_semi_snp and distance > near_falloff_distance)) then
 				break
-			elseif hit.unit:in_slot(shield_mask) and (hit.unit:name():key() == 'af254947f0288a6c' or hit.unit:name():key() == '15cbabccf0841ff8'  --Titan shields
-			or hit.unit:name():key() == '5deefee472c1903d' or hit.unit:name():key() == 'e26c602b7a43d7bb') and not can_shoot_through_titan_shield then --Marshall shields
+			elseif hit.unit:in_slot(shield_mask) and (
+				 --Titan shields
+				hit.unit:name():key() == 'af254947f0288a6c' or 
+				hit.unit:name():key() == '15cbabccf0841ff8' or 
+				hit.unit:name():key() == '1da6c7ac7ded3f9b' or
+				-- Marshall shields
+				hit.unit:name():key() == '5deefee472c1903d' or
+				hit.unit:name():key() == 'e26c602b7a43d7bb' or
+				hit.unit:name():key() == 'bd383b20175461fe'
+			) and not can_shoot_through_titan_shield then --Marshall shields
 				break
 			elseif hit.unit:in_slot(shield_mask) and hit.unit:name():key() == '4a4a5e0034dd5340' then --Winters being a shit.
 				break						
