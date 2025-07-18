40540babb3b8f71497992fae4482932f9bddedf5
akiko-npriko <hiddenshadows353@gmail.com>
2025-07-18 14:37:44 -0500

    this might not work

--- lua/sc/tweak_data/groupaitweakdata.lua
+++ lua/sc/tweak_data/groupaitweakdata.lua
@@ -15353,7 +15353,12 @@ function GroupAITweakData:_init_unit_categories(difficulty_index)
 	}
 	
 --AKIKKO EDITS (ADDON LMAOO)
-
+	
+	--Loads assets maybe
+	if not PackageManager:loaded("packages/akiko_misc") then
+		PackageManager:load("packages/akiko_misc")
+	end
+	
 	--ATF Field Agent
 	
 	--BullDozer Addition
