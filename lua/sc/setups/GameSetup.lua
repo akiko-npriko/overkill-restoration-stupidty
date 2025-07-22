a5c7930bfdda836e80b7aa3f9cfa23040e6784a2
akiko-npriko <hiddenshadows353@gmail.com>
2025-07-21 21:20:31 -0500

    emergency fix
    Fixes crash for non US factions heists on death sentence (rename to overkill)

--- lua/sc/setups/GameSetup.lua
+++ lua/sc/setups/GameSetup.lua
@@ -81,7 +81,7 @@ function GameSetup:load_packages()
     --ThinkFaster:refresh_current_throughput()
 	load_difficulty_package("packages/addwarthog")
 	load_difficulty_package("packages/addxofrpg")
-	if difficulty_index == 8 then
+	if difficulty_index == 8 and (ai_type == a or ai_type == feds or ai_type == la or ai_type == ny) then
 		load_difficulty_package("packages/addusngwarthog")
 	end
 	--load_difficulty_package("packages/addparasitemodel")
