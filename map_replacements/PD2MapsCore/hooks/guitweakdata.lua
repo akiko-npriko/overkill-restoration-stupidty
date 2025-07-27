7f2e5ce8c5b7a1ec35b309d1e7bc2fe274fb6dfd
akiko-npriko <hiddenshadows353@gmail.com>
2025-07-27 01:23:06 -0500

    Crime.Net :3

--- map_replacements/PD2MapsCore/hooks/guitweakdata.lua
+++ map_replacements/PD2MapsCore/hooks/guitweakdata.lua
@@ -4,12 +4,12 @@ Hooks:PostHook( GuiTweakData, "init", "pd2maps_contact", function(self)
 		name_id = "heist_contact_pd2maps",
 		{
 			desc_id = "heist_contact_pd2maps_desc",
-			video = "pd2m_codex1",
+			video = "cn_veritasgroup1",
 			post_event = nil
 		},
 		{
-			desc_id = "heist_contact_pd2maps_crew_desc",
-			video = "pd2m_codex1",
+			desc_id = "heist_contact_pd2maps_desc",
+			video = "cn_veritasgroup1",
 			post_event = nil
 		}
 	}
