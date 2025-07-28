6ac4b2885dd3fe0cd084cc90803b78c8a21e796e
akiko-npriko <hiddenshadows353@gmail.com>
2025-07-28 00:41:12 -0500

    fix ig

--- map_replacements/Flatline/hooks/russia_enemies.lua
+++ map_replacements/Flatline/hooks/russia_enemies.lua
@@ -1,4 +1,4 @@
-
+return
 if not Global.level_data or BeardLib.Utils:ModExists("RestorationMod") then return end
 
 Hooks:PostHook(GroupAITweakData, "_init_unit_categories", "russia_groupai", function(self, difficulty_index)
