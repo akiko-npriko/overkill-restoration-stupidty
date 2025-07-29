450ac4e3714280a80a55d5aa2c85b48050512aa9
akiko-npriko <hiddenshadows353@gmail.com>
2025-07-29 04:04:15 -0500

    Revert "Tempt #2"
    This reverts commit fbd50184bed5352eaade5722600557426b0d1bd4.

--- lua/sc/network/base/networkmanager.lua
+++ lua/sc/network/base/networkmanager.lua
@@ -5,8 +5,8 @@
 -- 		self:register_handler("RestorationMod__unit", RestorationMod__UnitNetworkHandler)
 -- 	end
 -- end)
-NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = "restoration_12.3.4.2_AkKscout"
-NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "restoration_12.3.4.2_AkKscout"
+NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = "restoration_12.3.4.2_FaNScout"
+NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "restoration_12.3.4.2_FaNScout"
 --Compatibility for BeardLib Editor just in case.
 if BLE and BLE:RunningFix() then
 	NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY  = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_editor"
