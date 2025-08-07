33a84f96e6613e900e5fcc9e58187be4d9e1d11b
akiko-npriko <hiddenshadows353@gmail.com>
2025-08-06 21:21:24 -0500

    Revert "i swear to god if that fixes everything"
    This reverts commit cbaa43c3fb7c64446692305f679e7a67396301de.

--- Corepre.lua
+++ Corepre.lua
@@ -3,7 +3,6 @@ RestorationMapFramework = RestorationMapFramework or class(MapFramework)
 
 RestorationMapFramework._directory = ModPath .. "map_replacements"
 RestorationMapFramework.type_name = "restoration"
-RestorationMapFramework.hidden = true
 
 RestorationMapFramework:init()
 RestorationMapFramework:InitMods()
