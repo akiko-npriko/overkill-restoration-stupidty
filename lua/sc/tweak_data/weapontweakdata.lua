f088b86a7da12bd999433ae1f214ede9750017a5
akiko-npriko <hiddenshadows353@gmail.com>
2025-08-07 16:52:19 -0500

    Updated Reload OFfset i think

--- lua/sc/tweak_data/weapontweakdata.lua
+++ lua/sc/tweak_data/weapontweakdata.lua
@@ -27840,7 +27840,10 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 				self.bromeo2.sounds.magazine_empty = "wp_rifle_slide_lock"
 				self.bromeo2.lock_slide = true
 				self.bromeo2.lock_slide_offset = 0.066
-				self.bromeo2.reload_offset = 0.033
+				self.bromeo2.reload_offset = {
+					reload = 0.033,
+					reload_not_empty = 0.033
+				}
 				self.bromeo2.animations.magazine_empty = nil
 				self.bromeo2.reload_speed_multiplier = 0.53
 				self.bromeo2.reload_not_empty_speed_multiplier = 1.2
@@ -27900,7 +27903,10 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 				self.bromeop.sounds.magazine_empty = "wp_rifle_slide_lock"
 				self.bromeop.lock_slide = true
 				self.bromeop.lock_slide_offset = 0.066
-				self.bromeop.reload_offset = 0.033
+				self.bromeop.reload_offset = {
+					reload = 0.033,
+					reload_not_empty = 0.033
+				}
 				self.bromeop.animations.magazine_empty = nil
 				self.bromeop.reload_speed_multiplier = 0.57
 				self.bromeop.reload_not_empty_speed_multiplier = 1.16
