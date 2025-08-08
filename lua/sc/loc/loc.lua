374847b975c92cf69bd35f2cfda03ac015a80b7f
akiko-npriko <hiddenshadows353@gmail.com>
2025-08-07 23:23:24 -0500

    Remove reduncdeyyy

--- lua/sc/loc/loc.lua
+++ lua/sc/loc/loc.lua
@@ -2759,8 +2759,6 @@ Hooks:Add("LocalizationManagerPostInit", "SC_Localization_Weapons_Eng", function
 				["bm_wp_mg42_m_75_desc"] = "Holds more rounds than physically possible by using the same technology to fit legs into jet engines.",
 				["bm_wp_mg42_m_100"] = "100rnd Drum",
 				["bm_wp_mg42_m_100_desc"] = "Holds even more rounds than physically possible by using the same technology to fit legs into jet engines.",
-				["bm_wp_mg42_b_vg38"] = "BlasTech DLT-19 Barrel",
-				["bm_wp_mg42_b_vg38_desc_sc"] = "Tech said to have come from #{skill_color}#a galaxy far, far away## converts this weapon to #{risk}#fire bolts of plasma## and utilize a #{skill_color}#recharging magazine.##\n\nRecharge delay: #{skill_color}#3s##\nRecharge rate: #{skill_color}#9/s## #{important_1}#(Halved while overheated)##\nOverheat penalty: #{important_1}#4s##",
 				--M134
 				["bm_m134_sc_desc"] = "\"Each step you take is paved with the corpses of your enemies...\"\n\n#{risk}#Has a brief spin-up period before firing; aiming keeps the weapon spun-up.##",
 				--Microgun
