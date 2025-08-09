29d8244a80ea8533077be4c4b040075ae352c43b
akiko-npriko <hiddenshadows353@gmail.com>
2025-08-09 00:06:20 -0500

    plz work

--- lua/sc/units/player/playerstandard.lua
+++ lua/sc/units/player/playerstandard.lua
@@ -2552,15 +2552,6 @@ function PlayerStandard:_get_melee_charge_lerp_value(t, offset)
 	return math.clamp(t - self._state_data.melee_start_t - offset, 0, max_charge_time) / max_charge_time
 end
 
-function terminatorcheck()
-	if managers.player:has_category_upgrade("player", "bullet_shield_knock") and managers.player:has_category_upgrade("carry", "movement_penalty_nullifier") and managers.player:has_category_upgrade("player", "resist_knockback_push") and managers.player:has_category_upgrade("player", "deflection_addend") and managers.player:has_category_upgrade("player", "health_multiplier") and managers.player:has_category_upgrade("player", "headshot_regen_armor_bonus") then
-		if self:upgrade_value("player", "deflection_addend", 0) > 0.09 and self:upgrade_value("player", "health_multiplier", 0) > 1.2 and self:upgrade_value("player", "headshot_regen_armor_bonus", 0) > 3.25 then
-			return true
-		end
-	end
-	return false
-end
-
 function PlayerStandard:_do_action_melee(t, input, skip_damage)
 	self._state_data.meleeing = nil
 	local melee_entry = managers.blackmarket:equipped_melee_weapon()
@@ -2748,6 +2739,14 @@ function PlayerStandard:_do_action_melee(t, input, skip_damage)
 
 		self._camera_unit:base():play_anim_melee_item(melee_item_tweak_anim, speed)
 		
+		local function terminatorcheck()
+			if managers.player:has_category_upgrade("player", "bullet_shield_knock") and managers.player:has_category_upgrade("carry", "movement_penalty_nullifier") and managers.player:has_category_upgrade("player", "resist_knockback_push") and managers.player:has_category_upgrade("player", "deflection_addend") and managers.player:has_category_upgrade("player", "health_multiplier") and managers.player:has_category_upgrade("player", "headshot_regen_armor_bonus") then
+				if self:upgrade_value("player", "deflection_addend", 0) > 0.09 and self:upgrade_value("player", "health_multiplier", 0) > 1.2 and self:upgrade_value("player", "headshot_regen_armor_bonus", 0) > 3.25 then
+					return true
+				end
+			end
+			return false
+		end
 		--TERMINATOR HANDS
 		local terminatorcheckerrr = terminatorcheck()
 		if terminatorcheckerrr then
