2626814d1aef643599c76cafe97f0602496ebda5
akiko-npriko <hiddenshadows353@gmail.com>
2025-08-16 22:52:25 -0500

    test

--- lua/sc/tweak_data/groupaitweakdata.lua
+++ lua/sc/tweak_data/groupaitweakdata.lua
@@ -15493,6 +15493,125 @@ function GroupAITweakData:_init_unit_categories(difficulty_index)
 	self.unit_categories.Shield_TIT_tank_assist = deep_clone(self.unit_categories.Shield_TIT_tank)
 	self.unit_categories.Shield_TIT_tank_assist.special_type = nil
 	
+	self.unit_categories.zeal_light = {
+		unit_types = {
+			america = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat/ene_zeal_swat")
+			},
+			russia = {
+				Idstring("units/pd2_mod_akiko/characters/ene_xof_light/ene_xof_light")
+			},
+			zombie = {
+				Idstring("units/pd2_mod_halloween/characters/ene_zeal_city_1/ene_zeal_city_1"),
+				Idstring("units/pd2_mod_halloween/characters/ene_zeal_city_2/ene_zeal_city_2"),
+				Idstring("units/pd2_mod_halloween/characters/ene_zeal_city_3/ene_zeal_city_3")
+			},
+			murkywater = {
+				Idstring("units/pd2_mod_akiko/characters/ene_xof_light/ene_xof_light")
+			},
+			federales = {
+				Idstring("units/pd2_mod_akiko/characters/ene_xof_light/ene_xof_light")
+			},					
+			nypd = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat/ene_zeal_swat")
+			},	
+			lapd = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat/ene_zeal_swat")
+			},
+			fbi = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat/ene_zeal_swat")
+			}
+		},
+		access = access_type_all
+	}
+	self.unit_categories.zeal_heavy = {
+		unit_types = {
+			america = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat_heavy/ene_zeal_swat_heavy")
+			},
+			russia = {
+				Idstring("units/pd2_mod_akiko/characters/ene_xof_heavy/ene_xof_heavy")
+			},
+			zombie = {
+				Idstring("units/pd2_mod_halloween/characters/ene_zeal_swat_heavy_sc/ene_zeal_swat_heavy_sc")
+			},
+			murkywater = {
+				Idstring("units/pd2_mod_akiko/characters/ene_xof_heavy/ene_xof_heavy")
+			},
+			federales = {
+				Idstring("units/pd2_mod_akiko/characters/ene_xof_heavy/ene_xof_heavy")
+			},					
+			nypd = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat_heavy/ene_zeal_swat_heavy")
+			},	
+			lapd = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat_heavy/ene_zeal_swat_heavy")
+			},
+			fbi = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat_heavy/ene_zeal_swat_heavy")
+			}
+		},
+		access = access_type_all
+	}
+	self.unit_categories.zeal_shield = {
+		unit_types = {
+			america = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat_shield/ene_zeal_swat_shield")
+			},
+			russia = {
+				Idstring("units/pd2_mod_akiko/characters/ene_xof_shield/ene_xof_shield")
+			},
+			zombie = {
+				Idstring("units/pd2_mod_halloween/characters/ene_zeal_swat_shield/ene_zeal_swat_shield")
+			},
+			murkywater = {
+				Idstring("units/pd2_mod_akiko/characters/ene_xof_shield/ene_xof_shield")
+			},
+			federales = {
+				Idstring("units/pd2_mod_akiko/characters/ene_xof_shield/ene_xof_shield")
+			},					
+			nypd = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat_shield/ene_zeal_swat_shield")
+			},	
+			lapd = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat_shield/ene_zeal_swat_shield")
+			},
+			fbi = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat_shield/ene_zeal_swat_shield")
+			}
+		},
+		access = access_type_all
+	}
+	self.unit_categories.zeal_taser = {
+		unit_types = {
+			america = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_tazer/ene_zeal_tazer")
+			},
+			russia = {
+				Idstring("units/pd2_mod_akiko/characters/ene_xof_volunteer/ene_xof_volunteer")
+			},
+			zombie = {
+				Idstring("units/pd2_mod_halloween/characters/ene_zeal_tazer/ene_zeal_tazer")
+			},
+			murkywater = {
+				Idstring("units/pd2_mod_akiko/characters/ene_xof_volunteer/ene_xof_volunteer")
+			},
+			federales = {
+				Idstring("units/pd2_mod_akiko/characters/ene_xof_volunteer/ene_xof_volunteer")
+			},					
+			nypd = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_tazer/ene_zeal_tazer")
+			},	
+			lapd = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_tazer/ene_zeal_tazer")
+			},
+			fbi = {
+				Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_tazer/ene_zeal_tazer")
+			}
+		},
+		access = access_type_all
+	}
+	
 	if difficulty_index == 5 then
 		akikomedicdozersdwbelow()
 	elseif difficulty_index == 6 then
