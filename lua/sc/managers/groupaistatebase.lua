5a7bc1f724ca1b40b3347ac43f416112397d5dd6
akiko-npriko <hiddenshadows353@gmail.com>
2025-08-20 22:06:33 -0500

    Add natural "Mode 13" to infinite loot heists
    - To be used for infinite loot heists primarily
    - Triggers Bravos on Pro Jobs when any diff increase would go over 100% (fifth assault onward, typically), much like Hardline mutator's new "Mode 13"
    - Threshold is customizable per heist if needed

--- lua/sc/managers/groupaistatebase.lua
+++ lua/sc/managers/groupaistatebase.lua
@@ -1769,9 +1769,16 @@ function GroupAIStateBase:set_difficulty(script_value, manual_value)
         end
     end
 
-    if not manual_value then
-        return
-    end
+	if not manual_value then
+		return
+	end
+
+	-- Check if this heist has natural Bravo spawns on Pro Jobs
+	local pro_job = Global.game_settings and Global.game_settings.one_down
+	local bravos_threshold = pro_job and restoration.natural_mode_13[job]
+	if bravos_threshold and bravos_threshold < self._difficulty_value + manual_value then
+		restoration.always_bravos = true
+	end
 
 	--note that this ADDS, not replaces. only way to replace is with a script_value of 0
     self._difficulty_value = math.min(self._difficulty_value + manual_value, 1)
