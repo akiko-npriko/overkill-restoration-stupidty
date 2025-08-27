0f84eb576f7f8fd5b636fac83425815e6b387829
akiko-npriko <hiddenshadows353@gmail.com>
2025-08-26 22:48:12 -0500

    Update playermanager.lua

--- lua/sc/managers/playermanager.lua
+++ lua/sc/managers/playermanager.lua
@@ -1821,8 +1821,8 @@ function PlayerManager:_attempt_adaptive_plate()
 		self.akiko_apc_give_armor_stage_thingy = 0
 		for i=1,stages,1 do
 			if cur_armor <= (armor_step*i) then
-				local numtoluvv = math.clamp((i+1), 0, stages)
-				self.akiko_apc_give_armor_thingy = (armor_step*numtoluvv)
+				local numtoluvv = math.clamp((i + 1), 1, stages)
+				self.akiko_apc_give_armor_thingy = (armor_step * numtoluvv)
 				self.akiko_apc_give_armor_stage_thingy = numtoluvv
 				break
 			end
