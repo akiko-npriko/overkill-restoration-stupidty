51d1e0a99a6e93e27e41f3c709cdc085108231bd
akiko-npriko <hiddenshadows353@gmail.com>
2025-08-29 23:14:49 -0500

    Update playerdamage.lua

--- lua/sc/units/player/playerdamage.lua
+++ lua/sc/units/player/playerdamage.lua
@@ -2421,9 +2421,13 @@ function PlayerDamage:get_adaptive_plate_stage_count()
 		
 		pm:_set_grenade({grenade = grenade, amount = math.min(amount, pm:get_max_grenades())})
 		]]
-		DelayedCalls:Add("refresh_apc_inst", 0.01, function()
-			pm:speed_up_grenade_cooldown((tweak_data.blackmarket.projectiles[managers.blackmarket:equipped_grenade()].base_cooldown) * throwables)
-		end)
+		
+		for i=1,throwables,1 do
+			DelayedCalls:Add("refresh_apc_inst", 0.25 * i, function()
+				pm:speed_up_grenade_cooldown((tweak_data.blackmarket.projectiles[managers.blackmarket:equipped_grenade()].base_cooldown))
+			end)
+		end
+		
 	end
 	
 	return stages
