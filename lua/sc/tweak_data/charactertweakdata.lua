5afc1abeac0f6159a331c0a132e8da663bb2b9bd
akiko-npriko <hiddenshadows353@gmail.com>
2025-08-31 22:08:55 -0500

    Minigun Dozer

--- lua/sc/tweak_data/charactertweakdata.lua
+++ lua/sc/tweak_data/charactertweakdata.lua
@@ -20263,7 +20263,8 @@ function CharacterTweakData:character_map()
 				"ene_bulldozer_taser",
 				"ene_zeal_bulldozer_taser",
 				"ene_ng_minigunner",
-				"ene_ng_bulldozer_medic"
+				"ene_ng_bulldozer_medic",
+				"ene_ng_bulldozer_minigun"
 			}
 		}
 
