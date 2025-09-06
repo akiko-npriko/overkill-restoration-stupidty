e40bd4feead4984a4a3beabaf6056f9a25f3414e
akiko-npriko <hiddenshadows353@gmail.com>
2025-09-06 14:45:52 -0500

    Update weaponfactorytweakdata.lua

--- lua/sc/tweak_data/weaponfactorytweakdata.lua
+++ lua/sc/tweak_data/weaponfactorytweakdata.lua
@@ -48389,8 +48389,7 @@ Hooks:PostHook( WeaponFactoryTweakData, "create_bonuses", "SC_mods", function(se
 				end
 			end
 		end
-		--For the lols
-		--[[
+
 		for i, part_id in pairs(self.wpn_fps_snp_kar98k.uses_parts) do
 			attachment_list = {
 				"wpn_fps_upg_a_slug",
@@ -48405,7 +48404,6 @@ Hooks:PostHook( WeaponFactoryTweakData, "create_bonuses", "SC_mods", function(se
 				end
 			end
 		end
-		]]
 		for i, part_id in pairs(self.wpn_fps_snp_kar98k.default_blueprint) do
 			attachment_list = {
 				"wpn_fps_snp_kar98k_rail"
