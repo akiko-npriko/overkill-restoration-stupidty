c89e82915cd8ac5ac8f76ccdff93ab13a0d46e42
akiko-npriko <hiddenshadows353@gmail.com>
2025-09-06 22:33:20 -0500

    AK15 "2020"
    6/7 :3

---
+++ map_replacements/!_AK15-2020_a/hooks/weaponfactorytweakdata.lua
@@ -0,0 +1,15 @@
+Hooks:PostHook(WeaponFactoryTweakData, "init", "gacha_frontline_ak15_factorytweakdata_init", function(self)
+
+	self:_clone_part_type_for_weapon("magazine", "wpn_fps_ass_ak15", 1)
+	
+
+
+
+
+
+
+
+
+
+	
+end)
