c36aafa3bbd306e808e5632a66655140f6d80028
akiko-npriko <hiddenshadows353@gmail.com>
2025-09-07 19:14:53 -0500

    Update charactertweakdata.lua

--- lua/sc/tweak_data/charactertweakdata.lua
+++ lua/sc/tweak_data/charactertweakdata.lua
@@ -20315,6 +20315,8 @@ function CharacterTweakData:character_map()
 				"ene_bulldozer_taser",
 				"ene_zeal_bulldozer_taser",
 				"ene_ng_minigunner",
+				"ene_ng_medic_1",
+				"ene_ng_medic_2",
 				"ene_ng_heavy_sniper",
 				"ene_ng_bulldozer_medic",
 				"ene_ng_bulldozer_minigun",
