e7eafa20f33c747463e73a0f7da51028ea3a6a62
akiko-npriko <hiddenshadows353@gmail.com>
2025-09-11 18:44:49 -0500

    Stupid fix

--- lua/sc/units/enemies/copbase.lua
+++ lua/sc/units/enemies/copbase.lua
@@ -1206,8 +1206,8 @@ local weapon_mapping = {
 	[("units/pd2_mod_dave/characters/ene_big_dave/ene_big_dave"):key{}] = dave.all_the_guns,
 -- US National Guards
 	[("units/pd2_mod_akiko/characters/ene_ng_medic_1/ene_ng_medic_1"):key{}] = national_guard.medic_rifle,
-	[("units/pd2_mod_akiko/characters/ene_ng_medic_2/ene_ng_medic_2"):key{}] = national_guard.medic_shotgun
-	[("units/pd2_mod_akiko/characters/ene_ng_cloaker/ene_ng_cloaker"):key{}] = national_guard.cloaker
+	[("units/pd2_mod_akiko/characters/ene_ng_medic_2/ene_ng_medic_2"):key{}] = national_guard.medic_shotgun,
+	[("units/pd2_mod_akiko/characters/ene_ng_cloaker/ene_ng_cloaker"):key{}] = national_guard.cloaker,
 }
 
 Hooks:PreHook(CopBase, "post_init", "MIX_post_init", function(self)
