f00cbe5e9cf9732dd7aaa49461229bcc702066fe
akiko-npriko <hiddenshadows353@gmail.com>
2025-09-14 13:37:08 -0500

    Update playermanager.lua

--- lua/sc/managers/playermanager.lua
+++ lua/sc/managers/playermanager.lua
@@ -1831,7 +1831,11 @@ function PlayerManager:_attempt_adaptive_plate()
 		end
 		
 		self.adaptive_plate_stage = math.clamp(self.adaptive_plate_stage - 1, 0, stages)
-		damage_ext:set_armor(math.clamp(cur_armor + armor_step, 0, max_armor))
+		if self.adaptive_plate_stage == stages then
+			damage_ext:set_armor(max_armor)
+		else
+			damage_ext:set_armor(math.clamp(cur_armor + armor_step, 0, max_armor))
+		end
 		--[[
 		self.adaptive_plate_stage = 0
 		damage_ext:set_armor(damage_ext:_max_armor())
