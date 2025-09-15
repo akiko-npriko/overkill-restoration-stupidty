c22e5c60f66c7d56641ea6847e777600c55d2cfd
akiko-npriko <hiddenshadows353@gmail.com>
2025-09-15 15:38:02 -0500

    Update Core.lua

--- Core.lua
+++ Core.lua
@@ -254,7 +254,6 @@ function restoration:Init()
 		"crojob2", --Bomb Dockyard
 		"friend", --Scarface Mansion
 		"kenaz", --Golden Grin Casino
-		"peta", --Goatsim 1
 		"watchdogs_2_day", --Watchdogs Day 2
 		"watchdogs_2", --Watchdogs Day 2 but night
 		"bex", --San Martin Bank
@@ -287,11 +286,12 @@ function restoration:Init()
 		"constantine_resort_lvl", --Scarlett Resort (Constantine Scores)
 		"constantine_murkyairport_lvl", --Murky Airport (Constantine Scores)
 		"Security_Avenue", --GenSec HQ Day 1
-        "arena_club30" -- Arena Orange
+		"arena_club30" -- Arena Orange
 	}
 	--Slightly reduced spawns, generally use for heists with lengthy sections where players typically hold out in one smallish position, or 'early game' heists.
 	restoration.tiny_levels = {
 		"welcome_to_the_jungle_2", --Big Oil 2. Scripted cloaker hell.
+		"peta", --Goatsim 1
 		"arena", -- Alesso Heist
 		"cane", --Santa's Workshop
 		"brb", --Brooklyn Bank
@@ -365,8 +365,6 @@ function restoration:Init()
 		"icing",	--Road Rage
 		"gunw_lvl",  -- Gun Waashing
 		"R&amp;B Bank" -- RNB Bank (Notoriety port)
-
-
 	}
 	--For levels that have aggressive scripted spawns, or spawn placement such that enemies are constantly spawned next to players.
 	restoration.very_tiny_levels = {
